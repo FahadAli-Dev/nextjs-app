cc686e7bd6bdff30042036318309f272f26963b8
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-07-02 20:04:23 +0500

    creat next.js practice repo

---
+++ app/about/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Thsi is about page</div>;
+}
+
+export default page;
---
+++ app/contact/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+  title: "contact page",
+  description: "This is contact page",
+};
+
+function page() {
+  return <div>This is contact page</div>;
+}
+
+export default page;
