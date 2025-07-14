bda50afec1ef8ae9458b4db033e168bdbf4f4794
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-07-14 17:18:17 +0500

    working on search Params

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -3,6 +3,7 @@ const Navbar = () => {
   return (
     <div>
       <ul className="bg-black text-white flex gap-[30px] justify-center font-bold py-[15px]">
+        <Link href={"/"}>Home</Link>
         <Link href={"/product"}>Product</Link>
       </ul>
     </div>
--- app/product/page.jsx
+++ app/product/page.jsx
@@ -1,9 +1,30 @@
+import Link from "next/link";
+
 const Product = async ({ searchParams }) => {
-  let data = await searchParams;
-  console.log(data);
+  let searchParam = await searchParams;
+  const URL = `https://dummyjson.com/products`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  const finalData = data.products;
+
+  console.log(finalData);
+  console.log(searchParam);
   return (
     <div className="text-center mt-[10px]">
-      <h1>search params</h1>
+      {finalData.map((item, idx) => {
+        return (
+          <div key={idx}>
+            <p></p>
+          </div>
+        );
+      })}
+
+      {/* <div className="flex gap-[20px] justify-center">
+        <Link href={"/product?category=shoes&price=100"}>shoes</Link>
+        <Link href={"/product?category=cloths&price=200"}>cloths</Link>
+      </div>
+      <p>category: {searchParam.category}</p>
+      <p>price: {searchParam.price}</p> */}
     </div>
   );
 };
